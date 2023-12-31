0fced13316d5df1baa5842ff93ad77853688129a
DDingdongho <bollmk13@gmail.com>
2023-12-31 13:15:28 +0900

    오류 확인 부탁드립니다.

---
+++ Source/StudyProject/Public/SUnrealObjectClass.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SUnrealObjectClass.generated.h"
+
+UCLASS()
+class STUDYPROJECT_API SUnrealObjectClass : public UObject
+{
+	GENERATED_BODY()
+};
